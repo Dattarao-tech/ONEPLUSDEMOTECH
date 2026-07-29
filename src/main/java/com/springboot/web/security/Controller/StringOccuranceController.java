13cdb26779fd5d09a7c4396835380c55d17c6fa1
dattaraodeosarkar4@gmail.com <dattaraodeosarkar4@gmail.com>
2026-07-29 17:34:28 +0530

    this is comit

--- src/main/java/com/springboot/web/security/Controller/StringOccuranceController.java
+++ src/main/java/com/springboot/web/security/Controller/StringOccuranceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,11 @@ import com.springboot.web.security.Sevice.StringOccuranceRequst;
 public class StringOccuranceController {
 	@Autowired
 	private StringOccuranceRequst stringOccuranceService;
+	
+	@GetMapping
+	public String get() {
+		return "Hello";
+	}
 	
     @SuppressWarnings("unchecked")
 	@PostMapping("/countOccurrences")
